5a40cfd5749f6abe9889cc6876d20ac1ff1a930d
ALMA080326 <aaltafakur@gmail.com>
2026-09-24 10:48:11 +0800

    Update firebaseConfig to include measurementId
    Added measurementId to firebaseConfig object.

--- firebase-config.js
+++ firebase-config.js
@@ -4,14 +4,14 @@
    File ini boleh di-upload ke GitHub. Ini BUKAN password; keamanan data
    diatur oleh "Rules" Realtime Database (lihat FIREBASE-SETUP.txt).
    ===================================================================== */
-const firebaseConfig = {
+const firebaseConfig = {  
   apiKey: "AIzaSyCkar1kZteBf35w-UoJEfrf7zQ6AxJ0cao",
   authDomain: "family100-online-fa8e0.firebaseapp.com",
-  databaseURL: "https://family100-online-fa8e0-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "family100-online-fa8e0",
   storageBucket: "family100-online-fa8e0.firebasestorage.app",
   messagingSenderId: "965235375216",
-  appId: 1:965235375216:web:3cc664b030547926e108b5"
+  appId: "1:965235375216:web:3cc664b030547926e108b5",
+  measurementId: "G-YXXVTXGBV4"
 };
 
 window.FAMILY100_FIREBASE_CONFIG = firebaseConfig;
