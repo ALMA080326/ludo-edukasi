ee609dcc39d3d43b2f04854090e404ecea924b47
ALMA080326 <aaltafakur@gmail.com>
2026-09-24 11:11:43 +0800

    Update Firebase configuration details
    Updated Firebase configuration with new API key and app ID.

--- firebase-config.js
+++ firebase-config.js
@@ -5,13 +5,13 @@
    diatur oleh "Rules" Realtime Database (lihat FIREBASE-SETUP.txt).
    ===================================================================== */
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY", // Silakan salin API Key Anda dari Firebase Console
+  apiKey: "AIzaSyCkar1kZteBf35w-UoJEfrf7zQ6AxJ0cao",
   authDomain: "family100-online-fa8e0.firebaseapp.com",
-  databaseURL: "https://family100-online-fa8e0-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "family100-online-fa8e0",
   storageBucket: "family100-online-fa8e0.firebasestorage.app",
   messagingSenderId: "965235375216",
-  appId: "YOUR_APP_ID" // Silakan salin App ID Anda dari Firebase Console
+  appId: "1:965235375216:web:3cc664b030547926e108b5",
+  measurementId: "G-YXXVTXGBV4"
 };
 
 window.FAMILY100_FIREBASE_CONFIG = firebaseConfig;
